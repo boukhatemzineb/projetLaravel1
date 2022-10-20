ee5abd296ab4aa4c183ffff4dae62b911c0c5cf6
Zineb <boukhatemzineb519@gmail.com>
2022-10-20 09:16:09 +0100

    first comit

---
+++ resources/views/test.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test </title>
+     <!-- Fonts -->
+     <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/js/app.js'])
+</head>
+<body>
+     <h1> Hey Zineb </h1>
+     <div class="container">
+  <div class="row">
+    <div class="col">
+      Column 1
+    </div>
+    <div class="col">
+      Column 2
+    </div>
+    <div class="col">
+      Column 3
+    </div>
+  </div>
+</div>
+<button type="button" class="btn btn-primary">Primary</button>
+<button type="button" class="btn btn-secondary">Secondary</button>
+<button type="button" class="btn btn-success">Success</button>
+<button type="button" class="btn btn-danger">Danger</button>
+<button type="button" class="btn btn-warning">Warning</button>
+<button type="button" class="btn btn-info">Info</button>
+<button type="button" class="btn btn-light">Light</button>
+<button type="button" class="btn btn-dark">Dark</button>
+
+<button type="button" class="btn btn-link">Link</button>
+</body>
+</html>
